ad538fe479c6100e2edd8d2d5aa990cb40daa08e
G100 <g100mylan229@gmail.com>
2023-12-30 17:46:14 +0800

    refactor: use <div> instead of <details>

--- composables/useDetailsWrapper.ts
+++ composables/useDetailsWrapper.ts
@@ -1,20 +1,53 @@
+import IconWrapped from '~/components/IconWrapped.vue'
 import type { ComponentOptions } from 'nuxt/dist/app/compat/capi'
 
 export function useDetailsWrapper(
   componentZh: ComponentOptions,
   componentEn: ComponentOptions,
 ) {
-  return {
+  return defineComponent({
     setup() {
       const markdown = ref()
       const { locale } = useI18n()
+      let isShow = ref(false)
+      let maxHeight: number
+      let containerDiv: HTMLDivElement | null = null
+
+      function onClick(event: ToggleEvent) {
+        isShow.value = !isShow.value
+        const target = event.currentTarget as HTMLDivElement
+        if (!containerDiv) {
+          containerDiv = target.querySelector(`[data-collapse]`)!
+          maxHeight = containerDiv.scrollHeight
+        }
+      }
+
       return () =>
-        h('details', [
-          h('summary', markdown.value?.frontmatter.title),
-          h(locale.value === 'en' ? componentEn : componentZh, {
-            ref: markdown,
-          }),
-        ])
+        h(
+          'div',
+          { class: 'group', onClick, open: isShow.value ? '' : undefined },
+          [
+            h('button', { class: 'flex' }, [
+              h(IconWrapped, {
+                name: 'loader-5-line',
+                class:
+                  'block rotate-180 transition-transform group-open:rotate-[270deg]',
+              }),
+              markdown.value?.frontmatter.title,
+            ]),
+            h(
+              'div',
+              {
+                class: 'transition-[max-height] overflow-hidden',
+                'data-collapse': '',
+                style: `max-height: ${isShow.value ? maxHeight : 0}px;`,
+              },
+              h(locale.value === 'en' ? componentEn : componentZh, {
+                ref: markdown,
+              }),
+            ),
+          ],
+        )
     },
-  }
+  })
 }
