6544720b1947adf5194db1ed6c7c7f15b3a6667f
G100 <g100mylan229@gmail.com>
2024-01-01 15:43:45 +0800

    refactor: update DetailsWrapper

--- composables/useDetailsWrapper.ts
+++ composables/useDetailsWrapper.ts
@@ -1,53 +1,82 @@
 import IconWrapped from '~/components/IconWrapped.vue'
 import type { ComponentOptions } from 'nuxt/dist/app/compat/capi'
 
-export function useDetailsWrapper(
-  componentZh: ComponentOptions,
-  componentEn: ComponentOptions,
-) {
+interface ArticleLangs {
+  en?: ComponentOptions
+  zh?: ComponentOptions
+}
+
+export function useDetailsWrapper(article: ArticleLangs) {
   return defineComponent({
     setup() {
-      const markdown = ref()
       const { locale } = useI18n()
+
+      if (!(locale.value in article)) {
+        return undefined
+      }
+
+      const markdown = ref()
       let isShow = ref(false)
       let maxHeight: number
       let containerDiv: HTMLDivElement | null = null
 
       function onClick(event: ToggleEvent) {
         isShow.value = !isShow.value
-        const target = event.currentTarget as HTMLDivElement
+        const button = event.currentTarget as HTMLDivElement
         if (!containerDiv) {
-          containerDiv = target.querySelector(`[data-collapse]`)!
+          containerDiv = button.parentNode!.querySelector(`[data-collapse]`)!
           maxHeight = containerDiv.scrollHeight
         }
       }
 
       return () =>
-        h(
-          'div',
-          { class: 'group', onClick, open: isShow.value ? '' : undefined },
-          [
-            h('button', { class: 'flex' }, [
+        h('div', { class: 'group', open: isShow.value ? '' : undefined }, [
+          h(
+            'button',
+            {
+              class: [
+                'text-1xl flex w-full rounded-md py-2 pl-1 pr-3',
+                'bg-gradient-to-b from-sundown-50 via-seagull-400 via-80% to-seagull-700',
+                'border-2 border-seagull-900',
+              ],
+              onClick,
+            },
+            [
               h(IconWrapped, {
                 name: 'loader-5-line',
                 class:
-                  'block rotate-180 transition-transform group-open:rotate-[270deg]',
+                  'rotate-180 transition-transform group-open:rotate-[0.75turn]',
               }),
               markdown.value?.frontmatter.title,
-            ]),
-            h(
-              'div',
-              {
-                class: 'transition-[max-height] overflow-hidden',
-                'data-collapse': '',
-                style: `max-height: ${isShow.value ? maxHeight : 0}px;`,
-              },
-              h(locale.value === 'en' ? componentEn : componentZh, {
-                ref: markdown,
-              }),
-            ),
-          ],
-        )
+            ],
+          ),
+          h(
+            'div',
+            {
+              class:
+                'transition-[max-height] overflow-hidden bg-gray-50/40 backdrop-blur-lg',
+              'data-collapse': '',
+              style: `max-height: ${isShow.value ? maxHeight : 0}px;`,
+            },
+            [
+              h(
+                article[locale.value as keyof ArticleLangs] as ComponentOptions,
+                {
+                  ref: markdown,
+                  class: 'p-3',
+                },
+              ),
+              h(
+                'button',
+                {
+                  class: 'h-10 w-full  text-3xl text-lochmara-950',
+                  onClick,
+                },
+                [h(IconWrapped, { name: 'arrow-up-double-line' })],
+              ),
+            ],
+          ),
+        ])
     },
   })
 }
